cf4a92a005c5ae8ddc5e43c87dc61000dad8af3f
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-03-12 21:16:01 -0500

    Challenge - upload pixel to graph

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 
 USERNAME = "astros"
 TOKEN = "asdlkvjanveruv"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
@@ -18,7 +19,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Running Graph",
     "unit": "M",
     "type": "float",
@@ -29,5 +30,15 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+pixel_config = {
+    "date": "20250312",
+    "quantity": "5"
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
 print(response.text)
